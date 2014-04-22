011de079a3badc09bb9b094ee9495c3aac702cc0
mfister <mike.fister@syntillamedical.com>
2014-04-21 20:04:01 -0700

    Mikes first real git commit

--- AndroClient/src/edu/palookaville/androclient/MainActivity.java
+++ AndroClient/src/edu/palookaville/androclient/MainActivity.java
@@ -11,6 +11,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+/* Mike Fister was here...*/
+
 public class MainActivity extends ActionBarActivity {
 
     @Override
