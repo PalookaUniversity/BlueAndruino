dd15192ba9fb4c119e5b54ff8d321117c102a788
George Vamos <gavamos@yahoo.com>
2014-07-04 18:33:19 -0700

    find local address

--- tserv/tserv.js
+++ tserv/tserv.js
@@ -4,16 +4,32 @@
 var http=require('http'),
     fs=require('fs'),
     os=require('os'),
+    ifaces=os.networkInterfaces(),
     express=require('express'),
     app = express(),
     context = {
       dRoot:'/data/tserv/dev/',
       scripts:'scripts/',
-      logs:'logs/'
+      logs:'logs/',
+      port:8000
      },
     files = fs.readdirSync(context.dRoot + context.scripts);
 
 //app.configure(function(){});
+
+for (var dev in ifaces) {
+  var alias=0;
+  ifaces[dev].forEach(function(details){
+    if (details.family=='IPv4') {
+      if (details.address !== '127.0.0.1'){
+	context.ipAddress=details.address;
+      }
+      ++alias;
+    }
+  });
+}
+
+context.url = 'http://'+context.ipAddress+":"+context.port;
 
 app.get('/scripts/:doc', function(req, res){
   var foo = fs.readFileSync(context.dRoot + context.scripts + '/' + req.params.doc);
@@ -29,5 +45,6 @@ app.get('/', function(req, res){
   res.send('hello world');
 });
 
-app.listen(8000);
-console.log("Server running at http://127.0.0.1:8000/");
+app.listen(context.port);
+
+console.log("Server running at "+context.url);
